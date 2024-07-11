c504ac895cf68d1f245171d2fc674cbdd3ebfbd5
boris.li <boris.li@zoom.us>
2024-07-11 11:23:40 +0800

    update manifest v2 to v3

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
+## Updates
+### 2024-07-11 
+- Manifest 升级到了 V3，避免 V2 版本的过期问题：https://developer.chrome.com/docs/extensions/develop/migrate/mv2-deprecation-timeline?hl=zh-cn
+- 增加了自动识别 Spotify 专辑类型的功能
+
+## Introduction
 在以下两个项目的基础上，增加了通过 Spotify 添加音乐条目的功能：
 - https://github.com/Sneexy/DoubanAddCloudMusicHelper
 - https://github.com/zeqianli/DoubanListingHelper
 
-使用方法：
+## Usage
 
 1. 在本页面点击 Code -> Download ZIP，下载 zip 文件到本地并解压，在 Chrome 浏览器中加载解压后的插件
 2. 在 Spotify 客户端中点击分享，复制 Album Link
@@ -12,6 +18,7 @@
 6. 点击左上角的 Collect
 7. 跳转到豆瓣音乐，检查信息填写有无错误，进入下一步，手动上传图片
 
-有任何使用问题请私信我：
+## Contact
+有任何使用问题请私信我，我会及时更新此插件：
 - douban: https://www.douban.com/people/araell/
 - weibo: https://weibo.com/u/7383494480
--- background.js
+++ background.js
@@ -3,10 +3,10 @@ console.log("background console starts");
 let meta = null; // metadata
 
 let getActiveTab = () => {
-    return browser.tabs.query({ active: true, currentWindow: true });
+    return chrome.tabs.query({ active: true, currentWindow: true });
 }
 
-browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     console.log("received content message.");
     switch (msg.page) {
         case "bandcamp":
@@ -15,22 +15,27 @@ browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
         case "163":
         case "spotify":
             // case "soundcloud": // TODO
-            console.log("Metadata received in background");
+            console.log("Metadata received in background: " + msg.meta);
+            if (msg.meta === undefined) {
+                console.log("msg.meta undefined.");
+                return;
+            }
             meta = JSON.parse(msg.meta);
             if (meta['imgUrl']) {
-                browser.downloads.download({ 'url': meta['imgUrl'] }
-                ).then(
-                    (id) => { console.log('Image downloaded'); },
-                    (error) => { console.log("Image download failed"); }
-                );
+                chrome.downloads.download({ url: meta['imgUrl'] }, (downloadId) => {
+                    if (chrome.runtime.lastError) {
+                        console.log("Image download failed: " + chrome.runtime.lastError.message);
+                    } else {
+                        console.log('Image downloaded with ID: ' + downloadId);
+                    }
+                });
             }
-
             break;
         case "douban-1":
             if (meta) {
-                console.log("Douban-1 message received and meta is stored in background.")
+                console.log("Douban-1 message received and meta is stored in background.");
                 getActiveTab().then((tabs) => {
-                    browser.tabs.sendMessage(tabs[0].id, { 'meta': JSON.stringify(meta) });
+                    chrome.tabs.sendMessage(tabs[0].id, { 'meta': JSON.stringify(meta) });
                     meta = null;
                 });
             }
